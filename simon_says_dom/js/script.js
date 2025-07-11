cb0ef953f6c7b20eac2f2770929c7bf8b96ea03e
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-11 18:41:29 +0200

    feat: working on the logic

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -5,22 +5,46 @@ const instructions = document.getElementById('instructions')
 const numbersList = document.getElementById('numbers-list')
 const answersForm = document.getElementById('answers-form')
 const inputGroup = document.getElementById('input-group')
+const message = document.getElementById('message')
 
-let counter = 10;
+// const uno = document.getElementById('uno').value
+// const due = document.getElementById('due').value
+// const tre = document.getElementById('tre').value
+// const quattro = document.getElementById('quattro').value
+// const cinque = document.getElementById('cinque').value
+
+const button = document.getElementById('btn')
+
+const NumeriRandom Math.floor(Math.random(1 , 50 ))
+
+let counterUno = 10;
 
 let timer;
 
 const FirstInterval = setInterval (() => { 
 
-  if(counter === 0 ){
+  if(counterUno === 0 ){
     clearInterval(FirstInterval)
     instructions.classList.add(`d-none`)
+    countDown.classList.add(`d-none`)
+    answersForm.classList.remove(`d-none`)
+
 
   }else {
-    countDown.innerText = counter
+    countDown.innerText = counterUno
+
     
   }
 
-  counter--
+  counterUno--
 
 }, 1000 );
+
+
+// button.addEventListener('click' , (e) => {
+
+
+
+
+
+// })
